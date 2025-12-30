79bb8a437e8ae99ddc7d616da327a4e1bd2a735e
ndholovu <ndholovu@gmail.com>
2025-12-30 11:21:43 +0900

    20251230

---
+++ main01.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import datetime
+
+app=FastAPI()
+templates=Jinja2Templates(directory='templates')
+
+@app.get('/')
+def page(request: Request):
+    return templates.TemplateResponse('gettime.html',{'request':request})
+
+@app.get('/button')
+def get_time(request: Request):
+    time=datetime.datetime.now().time()
+    return templates.TemplateResponse(
+        'time_fragment.html',{'request': request,'time':time}
+    )
